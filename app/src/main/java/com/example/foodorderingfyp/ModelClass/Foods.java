d9f2c1ea744bea76b22563f570a7ebc1bb55e473
chianjia <khngcj-pm18@student.tarc.edu.my>
2021-08-03 21:40:53 +0800

    Edit dependencies

--- app/src/main/java/com/example/foodorderingfyp/ModelClass/Foods.java
+++ app/src/main/java/com/example/foodorderingfyp/ModelClass/Foods.java
@@ -1,6 +1,6 @@
 package com.example.foodorderingfyp.ModelClass;
 
-public class Food {
+public class Foods {
 
     //follow firebase name that u want to retrieve all the information
    /* private String foodName,description,price,image;
