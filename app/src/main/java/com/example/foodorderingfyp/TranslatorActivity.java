135fd882096c06a889dddd67c5aba02bba4f7e6e
thang <gohtw-pm18@student.tarc.edu.my>
2021-11-20 00:16:28 +0800

    Commit New

---
+++ app/src/main/java/com/example/foodorderingfyp/TranslatorActivity.java
@@ -0,0 +1,14 @@
+package com.example.foodorderingfyp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TranslatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translator);
+    }
+}
